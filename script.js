5fa3b14f87dd8855aa8e04440123fcdd51393ffc
zazamarkoidze <122829784+zazamarkoidze@users.noreply.github.com>
2023-03-21 05:44:43 -0700

    finish

--- script.js
+++ script.js
@@ -2,5 +2,5 @@ let navigationElement = document.getElementById("barNav");
 let burgerElement = document.getElementById("burgerBar");
 
 burgerElement.addEventListener("click", function(){
-    navigationElement.classList.toggle("navActiv");
+   navigationElement.classList.toggle("navActiv");
 });
